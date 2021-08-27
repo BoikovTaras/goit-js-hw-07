46fbebae34a1f8ebe1f76bfff7cf6156632bbc36
Taras Boikov <tarasgowest@gmail.com>
2021-08-27 23:55:35 +0300

    добавляет первые изиенения в первую задачу

---
+++ src/js/task-01.js
@@ -0,0 +1,4 @@
+const itemsEl = document.querySelectorAll('.item');
+console.log('В списке', itemsEl.length, 'категории');
+const animalEl2 = document.querySelectorAll('.item h2');
+console.log(animalEl2)
