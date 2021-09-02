ea781873c8316c20693218dda4738f9d5fccadd4
Taras Boikov <tarasgowest@gmail.com>
2021-09-02 21:37:57 +0300

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -9,11 +9,11 @@ const ingredients = [
 
 const ingredientEl = ingredients.map(ingredient => {
   const liEl = document.createElement('li');
-  liEl.textContent = 'ingredient';
+  liEl.textContent = ingredient;
   return liEl;
 });
 console.log(ingredientEl);
 
-const ulEl = document.querySelector('.ingredients');
+const ulEl = document.querySelector('#ingredients');
 ulEl.append(...ingredientEl);
-console.log('ulEl');
+console.log(ulEl);
